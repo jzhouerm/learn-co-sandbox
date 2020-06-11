a12645d8ea876ee36f87277ea949b5a62471ac18
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 18:50:26 +0000

    Done.

---
+++ example_file.rb
@@ -0,0 +1,5 @@
+counter = 0
+until counter == 3
+  puts "The current number is less than 3."
+  counter += 1
+end
--- hello.rb
+++ hello.rb
@@ -1,1 +1,5 @@
-Print
+favorite_number = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+10.times do
+  favorite_number + 1
+end
+puts "And now your favorite_number is 10 more! It is #{favorite_number}"
